042b8ee163a5d1e86a2485c916548db05deb7083
Alisa McCormick <muntananuchat@gmail.com>
2018-08-31 17:31:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+ kittens.push("Ralph") 
+ return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.slice(-1)
+  return
+}
